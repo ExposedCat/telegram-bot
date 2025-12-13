74d478b9491fb0f0b32459f6fed5b9cfcaddb655
Artem Prokop <artem13.prokop@gmail.com>
2025-12-13 18:48:37 +0100

    feat: linting & formatting

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,17 @@
-import { Bot,Context as GrammyContext } from 'grammy';
+import { Bot, type Context as GrammyContext } from "grammy";
 import { stateComposer } from "./features/state.ts";
 
 export type Context = GrammyContext;
 
 export function initBot(token: string) {
-  const bot = new Bot(token);
+	const bot = new Bot(token);
 
-  bot.use(stateComposer);
+	bot.use(stateComposer);
 
-  return () => new Promise((resolve) =>
-		bot.start({
-			onStart: () => resolve(undefined),
-		}),
-	);
+	return () =>
+		new Promise((resolve) =>
+			bot.start({
+				onStart: () => resolve(undefined),
+			}),
+		);
 }
--- src/features/state.ts
+++ src/features/state.ts
@@ -1,9 +1,9 @@
 import { Composer } from "grammy";
-import { Context } from "../bot.ts";
+import type { Context } from "../bot.ts";
 
 export const stateComposer = new Composer<Context>();
 
-stateComposer.chatType("private").command("start", async(ctx) => {
+stateComposer.chatType("private").command("start", async (ctx) => {
 	await ctx.reply("Hello, world!");
 });
 
